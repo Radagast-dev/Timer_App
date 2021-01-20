39fb6566a2220b9903e5705eab5e266e70c9411a
Seba <seb.scheid92@gmail.com>
2021-01-20 18:04:01 +0100

    Start-Stop done, Todo input textbox, values abgleichen mit verstrichener zeit

--- Timer.xaml.cs
+++ Timer.xaml.cs
@@ -20,30 +20,58 @@ namespace Timer_App
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {            
+        DispatcherTimer disTimer = new DispatcherTimer();       //datum und Uhrzeit
         public MainWindow()
         {
             InitializeComponent();
         }
+        
+        private bool timerStarted = false;                      //bool für start-stopp bedingung festzulegen
 
-        private void timerFoo(object sender, EventArgs e)
+        private void timerStart(object sender, EventArgs e)     //eventmethode? start
         {
-            DispatcherTimer disTimer = new DispatcherTimer();
-            disTimer.Interval = new TimeSpan(0,0,1);
-            disTimer.Tick += new EventHandler(startButton_Click);
-            disTimer.Start();
+
+            disTimer.Interval = new TimeSpan(0,0,1);            //definiere angezeigtes zeitintervall
+            disTimer.Tick += new EventHandler(startButton_Click); //subscribe .tick an event handler
+            disTimer.Start();                                       //starte timer
+                
+            timerStarted = true;                                //setze bool auf true für stopp-bed
+
+            return;
+        }
+
+        private void timerStop(object sender, EventArgs e)
+        {
+            disTimer.Interval = new TimeSpan(0, 0, 1);
+            disTimer.Tick += new EventHandler(stopButton_Click);
+            disTimer.Stop();
+
+            timerStarted = false;
+
             return;
         }
 
         private void startButton_Click(object sender, EventArgs e)
         {
-            timerFoo(sender, e);
+            timerStart(sender, e);
             label.Content = DateTime.Now.ToString();
         }
 
-        private void stopButton_Click(object sender, RoutedEventArgs e)
+        private void stopButton_Click(object sender, EventArgs e)
         {
-
+            if(timerStarted == true)
+            {
+                try
+                {
+                    timerStop(sender, e);
+                    MessageBox.Show("Timer stopped!");
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.ToString());
+                }
+            }
         }
     }
 }
